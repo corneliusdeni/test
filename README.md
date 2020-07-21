a49ef33b0d464b283683d6376655b17c2bc65547
Cornelius Deni WP <corneliusdeniw@gmail.com>
2020-07-21 16:34:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# NovaTurny
+# NovaTurny
+# NovaTurny
+# WebGIS
---
+++ data/IbuKotaKecamatan_11.js
@@ -0,0 +1,24 @@
+var json_IbuKotaKecamatan_11 = {
+"type": "FeatureCollection",
+"name": "IbuKotaKecamatan_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Tempel" }, "geometry": { "type": "Point", "coordinates": [ 110.327201882171849, -7.651492630543489 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Turi" }, "geometry": { "type": "Point", "coordinates": [ 110.369699767493074, -7.65199957240921 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Pakem" }, "geometry": { "type": "Point", "coordinates": [ 110.420125138106371, -7.667222813241056 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Cangkringan" }, "geometry": { "type": "Point", "coordinates": [ 110.455511036233219, -7.658966975479862 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Ngemplak" }, "geometry": { "type": "Point", "coordinates": [ 110.444906222935813, -7.698081471988615 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Kalasan" }, "geometry": { "type": "Point", "coordinates": [ 110.467195243870989, -7.770049653462809 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Prambanan" }, "geometry": { "type": "Point", "coordinates": [ 110.489965927656257, -7.756256860013088 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Berbah" }, "geometry": { "type": "Point", "coordinates": [ 110.442831068965361, -7.805301916312144 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Depok" }, "geometry": { "type": "Point", "coordinates": [ 110.39370056035763, -7.758480042115901 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Mlati" }, "geometry": { "type": "Point", "coordinates": [ 110.328306533977184, -7.734196591886167 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Gamping" }, "geometry": { "type": "Point", "coordinates": [ 110.321927549622444, -7.795760563440812 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Godean" }, "geometry": { "type": "Point", "coordinates": [ 110.29977256123243, -7.7697503590225 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Moyudan" }, "geometry": { "type": "Point", "coordinates": [ 110.253762821946879, -7.772878661403386 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Minggir" }, "geometry": { "type": "Point", "coordinates": [ 110.234721537561128, -7.725566360996579 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Seyegan" }, "geometry": { "type": "Point", "coordinates": [ 110.308238042213816, -7.720906369188741 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Sleman" }, "geometry": { "type": "Point", "coordinates": [ 110.340774004293223, -7.684107856646608 ] } },
+{ "type": "Feature", "properties": { "NAMA": "Kecamatan Ngaglik" }, "geometry": { "type": "Point", "coordinates": [ 110.401008796799715, -7.723844602225085 ] } }
+]
+}
